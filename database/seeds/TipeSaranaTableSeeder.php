939fd528f4904c5c71adb8be970471a0a57b838b
felicia-christie <waxrain1217@gmail.com>
2015-04-28 19:11:50 +0700

    tah apalah yang perlu dikomit

--- database/seeds/TipeSaranaTableSeeder.php
+++ database/seeds/TipeSaranaTableSeeder.php
@@ -7,7 +7,6 @@ class TipeSaranaTableSeeder extends Seeder {
 
     public function run()
     {
-<<<<<<< HEAD
       /*  TipeSarana::create(['type' => 'Truk Compactor Besar', 'count' => 5]);
         TipeSarana::create(['type' => 'Truk Compactor Kecil', 'count' => 7]);
         TipeSarana::create(['type' => 'Dump Truck Besar', 'count' => 17]);
@@ -15,12 +14,6 @@ class TipeSaranaTableSeeder extends Seeder {
         TipeSarana::create(['type' => 'Arm Roll Besar', 'count' => 52]);
         TipeSarana::create(['type' => 'Arm Roll Kecil', 'count' => 11]);
         TipeSarana::create(['type' => 'Mobil Kecil', 'count' => 4]); */
-=======
-//        Tolong tambahkan daftar Tipe Sarana disini, cari minimal 5, maksimal 10.
-        TipeSarana::create(['type' => 'Truk A']);
-//        Yang di atas cuma contoh, diganti ya, kecuali emang itu namanya. countnya diisi random aja, asalkan masuk akal
->>>>>>> ca256a19b1dd2ff325e6068699d0992675e96194
-
         $sarana = array(
           ['type' => 'Truk Compactor Besar', 'count' => 5],
           ['type' => 'Truk Compactor Kecil', 'count' => 7],

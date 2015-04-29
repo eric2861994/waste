a62791e3dee8db2fac4e4af8cfbfc6b9a889f6b8
dalva24 <dariel24@gmail.com>
2015-04-29 21:56:47 +0700

    pengawasan with master pages

--- resources/views/jadwal/list.blade.php
+++ resources/views/jadwal/list.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 @extends('administrasi_master')
 
 @section('main-section')
@@ -141,30 +140,3 @@
         });
     </script>
 @stop
-=======
-@extends ('jadwal_master')
-
-@section('main-section')
-          <!--<h1 class="page-header">Dashboard</h1>-->
-          <h2 class="sub-header">Daftar Jadwal</h2>
-          <div class="table-responsive">
-            <table class="table table-striped">
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Nama</th>
-                  <th>Jadwal</th>
-                </tr>
-              </thead>
-              <tbody>
-				<tr>
-                  <td>TODO fill backend</td>
-                  <td>TODO fill backend</td>
-				  <td>TODO fill backend</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-
-@stop
->>>>>>> 8115e12ec52e6b64e08574180c9f055803bb48bb
